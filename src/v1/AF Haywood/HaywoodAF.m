4e012d19fbd699d97c17c25b0d19956abe75aa1f
tristynferreiro <tferreiro1@gmail.com>
2023-09-12 08:31:00 +0200

    Haywood AF - simple bug fix in DS rangebin # calculation

--- src/v1/AF Haywood/HaywoodAF.m
+++ src/v1/AF Haywood/HaywoodAF.m
@@ -5,7 +5,7 @@ function [AF_RA_HRRP] = HaywoodAF(RA_HRRP)
 [numProfiles,numRangeBins] = size(RA_HRRP);
 
 % Step 1: Calculate Eq A.8 of Zyweck's appendix - amplitude variance
-amplitudeVariance = 1/(numProfiles-1)*var(abs(RA_HRRP), [],1); % 1xM matrix
+amplitudeVariance = (1/(numProfiles-1))*var(abs(RA_HRRP), [],1); % 1xM matrix
 
 % Step 2: Identify scatterer (range bin) that satifies the dominant scatterer criteris. This is the minimum amplitude varying range bin in each range bin row
 % Criteria 1: power of scatterer>average power - Eq A.10 of Zyweck's appendix
@@ -14,19 +14,19 @@ averagePowerScatterer = mean(powerScatterer);
 % find possible scatterers where power of scatterer > average power
 candidateScatterersIdx = find(powerScatterer>averagePowerScatterer); % array of matches
 
-% Criteria 2: minimum variance is dominant scatterer (DS) - Eq A.9 of Zyweck's appendix
-[varianceDS,varianceDSidx] = min(amplitudeVariance(candidateScatterersIdx)); % returns index
+% Criteria 2: candidate with minimum variance is dominant scatterer (DS) - Eq A.9 of Zyweck's appendix
+[~,varianceDSidx] = min(amplitudeVariance(candidateScatterersIdx)); % returns index of match in candidateScatterersIdx
+DSidx = candidateScatterersIdx(varianceDSidx); % range bin number of DS
 
 % Step 3: Calculate phase differences between reference and other profiles - Eq A.11 of Zyweck's appendix
-phaseHistoryDS = angle(RA_HRRP(:,varianceDSidx)); % N x 1 matrix
-% phase_ref = imag(RA_HRRP(1,varianceDSidx)); % single value
-% phase_diff = RA_HRRP(:,varianceDSidx) - phase_ref % Gives Nx1 array
+phaseHistoryDS = angle(RA_HRRP(:,DSidx)); % N x 1 matrix
+disp(DSidx)
+% phase_ref = abs(RA_HRRP(:,DSidx)); 
+% phase_diff = abs(RA_HRRP) - phase_ref; % Gives Nx1 array
 
 % Step 4: apply phase differences to each HRRP - Eq A.12 of Zyweck's appendix
 complexConjugateDS = exp(-1i*phaseHistoryDS);
-% compensationMatrix = repmat(complexConjugateDS,1,numRangeBins)
-% AF_RA_HRRP_1 = RA_HRRP.*compensationMatrix
-AF_RA_HRRP = RA_HRRP.*complexConjugateDS; % Eq A.13 of Zyweck's appendix
-
-% AF_RA_HRRP = RA_HRRP.*exp(-1i*phase_diff);
+compensationMatrix = repmat(complexConjugateDS,1,numRangeBins);
+AF_RA_HRRP = RA_HRRP.*compensationMatrix; % Eq A.13 of Zyweck's appendix
+%AF_RA_HRRP = RA_HRRP.*complexConjugateDS; 
 end
