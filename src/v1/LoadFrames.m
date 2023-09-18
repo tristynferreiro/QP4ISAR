5e32606ef4de5e7eba907cc48bfcb3c4d7abf723
tristynferreiro <tferreiro1@gmail.com>
2023-09-18 17:05:14 +0200

    Multi Frame Processing - compute unfocused ISAR image of each frame

--- src/v1/LoadFrames.m
+++ src/v1/LoadFrames.m
@@ -1,7 +1,7 @@
-close all; 
-clear all;
-clc; 
-
+close all; clear all; clc; 
+% This implementation extracts the data in frames (groups of range bins).
+% It is an update on the original LoadRadarData.m which only processes a
+% single frame.
 %% Load CSIR dataset 
 
 load('DAP_2010-10-14_09-43-33_010_zayaan_inbound_singlebin_P455_G1_sb_HRR.mat');
@@ -21,42 +21,41 @@ colorbar;
 
 %% Slide window through all profiles (create frames)
 % Step 1: Define parameters
-CPTWL = 400;
-overlap = 0.8 * CPTWL;
+CPTWL = 128;
+overlap = 0.5 * CPTWL;
 
 % Step 2: Calculate the number of frames
 num_frames = 1 + (NumRangeBins-CPTWL)/(CPTWL-overlap); 
 
 % Step 3: Implement sliding window
-for frame = 1: num_frames
-    startProfile = (frame-1) * (CPTWL-overlap) + 1 ; 
-    stopProfile = startProfile + CPTWL-1;
+for framenum = 1: num_frames
+    startProfile = floor((framenum-1) * (CPTWL-overlap) + 1 ); 
+    stopProfile = floor(startProfile + CPTWL-1);
     
     ProfilesToProcess = stopProfile - startProfile;
     DopplerAxis_Hz = (-ProfilesToProcess/2:1:ProfilesToProcess/2-1)*ProfileRepetitionFreq/ProfilesToProcess;
 
     % Plot Unaligned HRR Profiles (single frame)
     HRR_profiles = circshift(HRRProfilesAll(startProfile:stopProfile, :), [0 50]);
-    figure; imagesc(sb_HRR.G1.xaxis_downrange_m, 1:size(HRR_profiles,1), 20*log10(abs(HRR_profiles))); 
-    xlabel('Range (m)');
-    ylabel('Profile Number');
-    title('Subset of HRR profiles');
-    colormap('jet');
-    colorbar;
+    % figure; imagesc(sb_HRR.G1.xaxis_downrange_m, 1:size(HRR_profiles,1), 20*log10(abs(HRR_profiles))); 
+    % xlabel('Range (m)');
+    % ylabel('Profile Number');
+    % title('Subset of HRR profiles');
+    % colormap('jet');
+    % colorbar;
 
+    %% Plot unfocused ISAR image - no range alignment, no autofocus
+    % apply hamming window function
+    window = hamming(size(ProfilesToProcess,1));
 
-    % %% Plot unfocused ISAR image - no range alignment, no autofocus
-    % % apply hamming window function
-    % window = hamming(size(ProfilesToProcess,1));
-    % 
-    % ISAR_image = fftshift(fft(HRR_profiles.*window,[],1),1); % Apply FFT in the slow-time dimension                    
-    % 
-    % ISAR_image_dB = Normalise_limitDynamicRange_ISAR_dB(ISAR_image, 35);    % Limit dynamic range of ISAR image 
-    % 
-    % % Plot unfocused ISAR image
-    % figure; imagesc( Range_axis, DopplerAxis_Hz, ISAR_image_dB );                                
-    % colorbar; xlabel('Range(m)'); axis ij; ylabel('Doppler frequency (Hz)')                                           
-    % title('Unfocused ISAR Image (no RA, no AF)'); axis xy; colormap('jet')
+    ISAR_image = fftshift(fft(HRR_profiles.*window,[],1),1); % Apply FFT in the slow-time dimension                    
+
+    ISAR_image_dB = Normalise_limitDynamicRange_ISAR_dB(ISAR_image, 35);    % Limit dynamic range of ISAR image 
+
+    % Plot unfocused ISAR image
+    figure; imagesc( Range_axis, DopplerAxis_Hz, ISAR_image_dB );                                
+    colorbar; xlabel('Range(m)'); axis ij; ylabel('Doppler frequency (Hz)')                                           
+    title('Unfocused ISAR Image (no RA, no AF)'); axis xy; colormap('jet')
    
 end
 
